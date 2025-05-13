3cf04aa650483957eb566e91c004954251304a7e
Abraham Jose Kazerian <abrahamK1834@hotmail.com>
2025-05-13 13:31:36 -0300

    correcting issue with wrong id

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
-import { GameHistory } from "../data/heardles";
+import { GameHistory, HeardleConfig, heardles } from "../data/heardles";
 
 
 
 export function Historical({ history }: { history: GameHistory[] }) {
+
+const getHeardles = (id: string): string =>{
+    const config = heardles.find((heardle) => heardle.id === id);
+    console.assert(config, `Heardle config not found for id: ${id}`);
+    return config!.title;
+}
   return (
     <div className="history">
       <h2>Previous Games</h2>
       {history.map((game) => (
         <div key={game.date} className="game">
-          <h3>{game.heardleId}</h3>
+          <h3>{getHeardles(game.heardle)}</h3>
           <p>{game.date}</p>
           <p>{game.game.guesses.join(", ")}</p>
         </div>
