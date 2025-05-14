6d445e29542aa7b8e018dcff89eaf5ac45349a33
Abraham Jose Kazerian <abrahamK1834@hotmail.com>
2025-05-13 21:34:48 -0300

    adding what was the winning song

--- src/components/history/history.tsx
+++ src/components/history/history.tsx
@@ -18,14 +18,15 @@ export function Historical({ history = [] }: { history: GameHistory[] }) {
   return (
     <div className="bg-gray-800 rounded-lg p-4 sm:p-6 mb-6">
       <h2>Previous Games</h2>
-      {history.map((game) => (
-        <div key={game.date} className="bg-gray-700 m-2 rounded-lg px-2 py-1 game">
+      {history.map((state) => (
+        <div key={state.date} className="bg-gray-700 m-2 rounded-lg px-2 py-1 game">
           <div className="flex flex-row justify-between items-center">
-            <h3 className={game.win? "text-green-500": "text-red-500"}><b>{getHeardles(game.heardle)}</b></h3><p><b>{formatDate(game.date)}</b></p></div>
+            <h3 className={state.win? "text-green-500": "text-red-500"}><b>{getHeardles(state.heardle)}</b></h3><p><b>{formatDate(state.date)}</b></p></div>
           <div>
+            <div><b>Answer:</b> {state.game.currentSong.title}</div>
             <div>Guesses:</div>
             <ol className="px-3">
-              {game.game.guesses.map((guess, index) => (
+              {state.game.guesses.map((guess, index) => (
                 <li key={index} className="guess">
                   {index + 1}: {guess}
                 </li>
